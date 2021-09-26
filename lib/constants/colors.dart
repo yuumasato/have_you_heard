ab97bae6d6bdcbf7c526d579aeb8005fdb993592
nilsonsasaki <nilsonsasaki@gmail.com>
2021-09-26 13:27:58 -0300

    created colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//players
+const Color kPlayer_1 = Color(0xFFFD86B1);
+const Color kPlayer_2 = Color(0xFFF5A35A);
+const Color kPlayer_3 = Color(0xFFEEC750);
+const Color kPlayer_4 = Color(0xFF41CA7B);
+const Color kPlayer_5 = Color(0xFF65C3C9);
+const Color kPlayer_6 = Color(0xFF59A6EA);
+
+//buttons
+const Color kPinkButton = Color(0xFFC64875);
+const Color kYellowButton = Color(0xFFF2CF21);
+
+//Background
+const Color kBackgroundDarkestGray = Color(0xFF30333A);
+const Color kBackgroundDarkerGray = Color(0xFF3F4548);
+const Color kBackgroundDarkGray = Color(0xFF464C50);
