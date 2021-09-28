0aac4fd8a7b79d9f97e5f155f5b7e0caa791a165
Angélica Nunes <angelica.nunes@usp.br>
2021-09-27 22:45:56 -0300

    Add locale strings (langs)

---
+++ lib/constants/locale_string.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+
+class LocalString extends Translations{
+  @override
+  // TODO: implement keys
+  Map<String, Map<String, String>> get keys => {
+    'pt_BR':{
+      'entrar': 'Entrar',
+      'criarSala':'Criar sala privada'
+    },
+    'es_AR':{
+      'entrar': 'Entrar',
+      'criarSala':'Crear sala privada'
+    }
+  };
+
+}
