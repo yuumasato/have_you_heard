99bb98517efad1298a39a2ab203a60c676ccf235
nilsonsasaki <nilsonsasaki@gmail.com>
2021-09-28 23:23:29 -0300

    updated app to dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:have_you_heard/constants/colors.dart';
 import 'package:have_you_heard/router/parser.dart';
 import 'package:provider/provider.dart';
 import 'package:get/get.dart';
@@ -7,27 +8,27 @@ import 'constants/locale_string.dart';
 import 'game_state.dart';
 
 void main() {
-  runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => HyhState()),
-          ChangeNotifierProvider(create: (context) => GameState()),
-        ],
-        child: const Hyh(),
-      )
-  );
-  }
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => HyhState()),
+      ChangeNotifierProvider(create: (context) => GameState()),
+    ],
+    child: const Hyh(),
+  ));
+}
 
 class Hyh extends StatelessWidget {
-  const Hyh({Key? key}): super(key: key);
+  const Hyh({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kBackgroundDarkestGray,
+        ),
         translations: LocalString(),
-        locale: Locale('pt','BR'),
+        locale: Locale('pt', 'BR'),
         onGenerateRoute: HyHRouter.generateRoute,
-        initialRoute: '/splash'
-
-    );
+        initialRoute: '/splash');
   }
 }
