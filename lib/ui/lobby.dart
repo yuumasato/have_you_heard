fb00ccbced70f830ac425ae6778db69e43c4095e
Angélica Nunes <angelica.nunes@usp.br>
2021-10-01 00:31:32 -0300

    Add lobby ui fix

--- lib/ui/lobby.dart
+++ lib/ui/lobby.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:have_you_heard/constants/colors.dart';
 
 import 'package:have_you_heard/game_state.dart';
 import 'package:have_you_heard/ui/room.dart';
@@ -17,6 +18,10 @@ class LobbyScreen extends StatefulWidget {
 
 class _LobbyScreenState extends State<LobbyScreen> {
   final myController = TextEditingController();
+  final appBar = AppBar(
+    backgroundColor: kBackgroundDarkestGray,
+    automaticallyImplyLeading: false,
+  );
 
   @override
   void initState() {
@@ -31,12 +36,14 @@ class _LobbyScreenState extends State<LobbyScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope (
+    final screenHeight = MediaQuery.of(context).size.height;
+    final appBarHeight = appBar.preferredSize.height;
+    final statusBarHeight = MediaQuery.of(context).padding.top;
+    return WillPopScope(
       onWillPop: () async {
         await showDialog(
             context: context,
-            builder: (_) =>
-                AlertDialog(
+            builder: (_) => AlertDialog(
                   title: const Text('Sair do jogo?'),
                   actions: [
                     ElevatedButton(
@@ -52,51 +59,125 @@ class _LobbyScreenState extends State<LobbyScreen> {
         return false;
       },
       child: Scaffold(
+        backgroundColor: kBackgroundDarkestGray,
+        appBar: appBar,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Consumer<HyhState> (
-                  builder: (context, hyh, child) {
-                    return Row(
-                        children: [
-                          const Text('Lobby'
-                          ),
-                          Icon(Icons.network_check,color: hyh.socketColor, size: 20.0),
-                        ]
-                    );
-                  }
+              Container(
+                margin: EdgeInsets.only(bottom:20),
+                height: (screenHeight - appBarHeight - statusBarHeight) * 0.07,
+                color: kBackgroundDarkGray,
+                alignment: Alignment.center,
+                child: const Text(
+                  'Escolha seu modo de jogo',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                      margin: const EdgeInsets.all(10.0),
-                      width: 200.0,
-                      height: 38.0,
-                      child: TextField(
-                          controller: myController,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Insira o código da sala',
-                          ))
+              Expanded(
+                flex:3,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            margin: const EdgeInsets.all(10.0),
+                            height: 38,
+                            width: 200.0,
+                            child: TextField(
+                                controller: myController,
+                                decoration: const InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.grey),
+                                  ),
+                                  labelText: 'Insira o código da sala',
+                                  labelStyle: TextStyle(fontSize: 16, color: Colors.white,fontWeight: FontWeight.w300),
+                                  hintStyle: TextStyle(color: Colors.white),
+                                  focusColor: Colors.red,
+                                  focusedBorder:OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                                  ),
+                                ),
+                              style: const TextStyle(color: Colors.white, decoration: TextDecoration.none),
+                            ),
+                        ),
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(primary:kYellowButton),
+                            onPressed: () {
+                              String roomID = myController.text;
+                              Provider.of<HyhState>(context, listen: false).roomID =
+                                  int.parse(roomID);
+                              Navigator.pushNamed(context, RoomScreen.routeName,
+                                  arguments: roomID);
+                            },
+                            child:
+                            Text(
+                              'entrar'.tr,
+                              style: const TextStyle(fontSize: 16, color: kBackgroundDarkestGray),
+                            )),
+                      ],
+                    ),
+                    Container(
+                      width: 200,
+                      child:
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              width: 50,
+                              child: const Divider(
+                                color: Colors.white,
+                              ),
+                            ),
+                            const Text(
+                              'ou',
+                              style: TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                            Container(
+                              width: 50,
+                              child: const Divider(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                    ),
+
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(primary:kYellowButton),
+                        onPressed: () {
+                          // Requisitar ao servidor criação de nova sala
+                          // TODO: criar GameState e passar pra RoomScreen
+                          String roomID =
+                          Provider.of<HyhState>(context, listen: false)
+                              .roomID
+                              .toString();
+                          Navigator.pushNamed(context, RoomScreen.routeName,
+                              arguments: roomID);
+                        },
+                        child:
+                        Text(
+                          'criarSala'.tr,
+                          style: const TextStyle(fontSize: 16, color: kBackgroundDarkestGray),
+                        )),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/full_landscape.png"),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.topCenter,
+                      ),
+                    ),
                   ),
-                  ElevatedButton(
-                      onPressed: () {
-                        String roomID = myController.text;
-                        Provider.of<HyhState>(context, listen: false).roomID = int.parse(roomID);
-                        Navigator.pushNamed(context, RoomScreen.routeName, arguments: roomID);
-                      },
-                      child: Text('entrar'.tr)),
-                ],
               ),
-              ElevatedButton(
-                  onPressed: () {
-                    // Requisitar ao servidor criação de nova sala
-                    // TODO: criar GameState e passar pra RoomScreen
-                    String roomID = Provider.of<HyhState>(context, listen: false).roomID.toString();
-                    Navigator.pushNamed(context, RoomScreen.routeName, arguments: roomID);
-                  },
-                  child: Text('criarSala'.tr)),
             ],
           ),
         ),
