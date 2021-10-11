706a6225b79c0b10c742cae0e4ed24b34a42017d
nilsonsasaki <nilsonsasaki@gmail.com>
2021-10-11 15:24:55 -0300

    Created game_winner layout

--- lib/ui/game_winner.dart
+++ lib/ui/game_winner.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:have_you_heard/controller/game_controller.dart';
+import 'package:have_you_heard/widgets/gray_stripe.dart';
+import 'package:have_you_heard/constants/colors.dart';
 
 import 'room.dart';
 
@@ -17,10 +19,29 @@ class GameWinnerScreen extends StatefulWidget {
 }
 
 class _GameWinnerScreenState extends State<GameWinnerScreen> {
+  final GameController gc = Get.find();
+  Timer? timer;
+
+  int count = 0;
+  List<int> votes = [1, 2, 5, 7, 3, 0];
+  int mostVotes = 0;
+  List<double> votesValues = [0.015, 0.015, 0.015, 0.015, 0.015, 0.015];
+
+  List<Color> playerColors = [
+    kPlayer_1,
+    kPlayer_2,
+    kPlayer_3,
+    kPlayer_4,
+    kPlayer_5,
+    kPlayer_6
+  ];
+
   @override
   initState() {
     super.initState();
+    getMostVotes();
     startTime();
+    startAnimationTimer();
   }
 
   startTime() async {
@@ -28,63 +49,131 @@ class _GameWinnerScreenState extends State<GameWinnerScreen> {
     return Timer(duration, route);
   }
 
+  startAnimationTimer() async {
+    var duration = const Duration(seconds: 1);
+    return Timer(duration, animationProgressTimer());
+  }
+
+  animationProgressTimer() async {
+    timer = Timer.periodic(
+        const Duration(milliseconds: 10), (_) => setVotesValues());
+  }
+
+  setVotesValues() {
+    setState(() {
+      for (int i = 0; i < votes.length; i++) {
+        if (votes[i] != 0) {
+          votesValues[i] = (votes[i] * count) / (mostVotes * 100);
+        }
+      }
+    });
+    if (count > 100) {
+      timer?.cancel();
+    }
+    count++;
+  }
+
+  getMostVotes() {
+    for (var value in votes) {
+      if (value > mostVotes) {
+        mostVotes = value;
+      }
+    }
+  }
+
   route() async {
-    final GameController gc = Get.find();
     await showDialog(
         context: context,
         barrierDismissible: false,
-        builder: (_) =>
-            WillPopScope(
-              // A choice must be made in this dialog, let's block the back button
-                onWillPop: () async => false,
-                child: SimpleDialog(
-                  title: const Text('Parabéns'),
-                  children: <Widget>[
-                    Image.asset('assets/images/trophy.png'),
-                    ElevatedButton(
-                        onPressed: () {
-                          // Reinstate a new Game State
-                          gc.reset();
-                          String roomID = gc.roomID;
-                          Get.offAllNamed("${RoomScreen.route}/$roomID");
-                        },
-                        child: const Text('Jogar Novamente')),
-                    TextButton(
-                        onPressed: () {
-                          gc.exitGame();
-                        },
-                        child: const Text('Finalizar partida'))
-                  ],
-                )));
+        builder: (_) => WillPopScope(
+            // A choice must be made in this dialog, let's block the back button
+            onWillPop: () async => false,
+            child: SimpleDialog(
+              title: const Center(child: Text('Parabéns')),
+              titleTextStyle: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: kYellowButton),
+              children: <Widget>[
+                Image.asset('assets/images/trophy.png'),
+                ElevatedButton(
+                    onPressed: () {
+                      // Reinstate a new Game State
+                      gc.reset();
+                      String roomID = gc.roomID;
+                      Get.offAllNamed("${RoomScreen.route}/$roomID");
+                    },
+                    child: const Text('Jogar Novamente')),
+                TextButton(
+                    onPressed: () {
+                      gc.exitGame();
+                    },
+                    child: const Text('Finalizar partida'))
+              ],
+            )));
   }
 
   @override
   Widget build(BuildContext context) {
-    final GameController gc = Get.find();
+    var screenWidth = MediaQuery.of(context).size.width;
+    final appBarHeight = AppBar().preferredSize.height;
+
     return WillPopScope(
-      // This is short lived screen, let's block the back button
+        // This is short lived screen, let's block the back button
         onWillPop: () async {
           startTime();
           return false;
         },
         child: Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+            ),
             body: SafeArea(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text('Ranking'),
-                    const Text('Nickname 4\nVencedor!'),
-                    for (var player in gc.game.playerList) buildPlayerButton(player.name),
-                  ],
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const GrayStripe(text: 'Ranking'),
+                Container(
+                  padding: EdgeInsets.only(
+                      bottom: appBarHeight * 0.50,
+                      left: screenWidth * 0.1,
+                      right: screenWidth * 0.1),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: appBarHeight * 0.50),
+                        child: const Text(
+                          'Nickname 4\nVencedor!',
+                          style: TextStyle(
+                              fontSize: 32, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      for (var index = 0; index < 6; index++)
+                        buildPlayerButton(index),
+                    ],
+                  ),
                 )
-            )
-        )
-    );
+              ],
+            ))));
   }
 
-  Widget buildPlayerButton(String name) {
-    return ElevatedButton(
-        onPressed: null,
-        child: Text(name));
+  Widget buildPlayerButton(int index) {
+    final appBarHeight = AppBar().preferredSize.height;
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: appBarHeight * 0.13),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        child: Stack(alignment: AlignmentDirectional.center, children: [
+          LinearProgressIndicator(
+            value: votesValues[index],
+            color: playerColors[index],
+            minHeight: appBarHeight * 0.66,
+            backgroundColor: kBackgroundDarkGray,
+          ),
+          Obx(() => Text(gc.game.playerList[index].name)),
+        ]),
+      ),
+    );
   }
 }
