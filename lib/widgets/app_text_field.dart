e8540dec98edc19793e545ef0b4393fb2099c3ad
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-02 09:08:59 -0300

    Created AppTextField

---
+++ lib/widgets/app_text_field.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:have_you_heard/constants/colors.dart';
+
+class AppTextField extends StatelessWidget {
+  const AppTextField({
+    Key? key,
+    required this.textEditingController,
+    required this.labelText,
+    this.autofocus = true,
+  }) : super(key: key);
+
+  final TextEditingController textEditingController;
+  final String labelText;
+  final bool autofocus;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      autofocus: autofocus,
+      controller: textEditingController,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: kGrayScaleDarkest,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: const BorderSide(color: Color(0xFF727E86), width: 2.0),
+        ),
+        labelText: labelText,
+        labelStyle: const TextStyle(
+            fontSize: 16,
+            color: kGrayScaleMediumLight,
+            fontWeight: FontWeight.w400),
+        hintStyle: const TextStyle(color: Color(0xFF727E86)),
+        //TODO: Unique color use #727E86 - Ask Design Team
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: const BorderSide(color: Color(0xFF727E86), width: 2.0),
+        ),
+      ),
+      style: const TextStyle(
+          color: kGrayScaleLightest, fontSize: 16, fontWeight: FontWeight.w700),
+    );
+  }
+}
