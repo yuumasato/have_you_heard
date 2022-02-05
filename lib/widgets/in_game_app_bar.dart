a639dd2657d742c6ba671ee8e6a877bd71fa8c18
nilsonsasaki <nilsonsasaki@gmail.com>
2022-02-04 21:51:30 -0300

    added new behaviour to icon buttons

--- lib/widgets/in_game_app_bar.dart
+++ lib/widgets/in_game_app_bar.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:get/get.dart';
 import 'package:have_you_heard/constants/colors.dart';
+import 'package:have_you_heard/ui/lobby.dart';
+import 'package:have_you_heard/ui/settings.dart';
 
 class InGameAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   final Size preferredSize;
   final String title;
-  final VoidCallback? onSettingsPressed;
-  final VoidCallback? onLeavePressed;
   final Color textColor;
   final FontWeight fontWeight;
 
   const InGameAppBar({
     this.title = '',
-    this.onSettingsPressed,
-    this.onLeavePressed,
     this.textColor = kGrayScaleLight,
     this.fontWeight = FontWeight.w400,
     Key? key,
@@ -33,11 +32,11 @@ class InGameAppBar extends StatelessWidget implements PreferredSizeWidget {
         icon: const Image(
           image: Svg("assets/images/icon_leave.svg", size: Size(24, 24)),
         ),
-        onPressed: onLeavePressed,
+        onPressed: (){Get.toNamed(LobbyScreen.route);}
       ),
       actions: [
         IconButton(
-          onPressed: onSettingsPressed,
+          onPressed: () {Get.toNamed(SettingsScreen.route);},
           icon: const Icon(Icons.settings_outlined),
         )
       ],
