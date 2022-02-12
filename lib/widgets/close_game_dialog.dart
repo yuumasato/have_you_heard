dbf950f8e29e3463ca29cd40094d25f77389ae5c
nilsonsasaki <nilsonsasaki@gmail.com>
2022-02-12 09:45:35 -0300

    swapped buttons types

--- lib/widgets/close_game_dialog.dart
+++ lib/widgets/close_game_dialog.dart
@@ -15,18 +15,28 @@ class CloseGameDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      title: Center(child: Text('leaveTheGame?'.tr,)),
-      titlePadding: const EdgeInsets.only(top: 20,bottom: 40,),
+      title: Center(
+          child: Text(
+        'leaveTheGame?'.tr,
+      )),
+      titlePadding: const EdgeInsets.only(
+        top: 20,
+        bottom: 40,
+      ),
       titleTextStyle: HyhTextStyle.heading24Bold,
       actionsAlignment: MainAxisAlignment.spaceBetween,
       actionsPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       actions: [
-        TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: Text('cancel'.tr, style: HyhTextStyle.body16BoldYellow,)),
         AppButton(
-            text: 'close'.tr,
-            onPressed: () =>SystemNavigator.pop(),
+          text: 'cancel'.tr,
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        TextButton(
+          onPressed: () => SystemNavigator.pop(),
+          child: Text(
+            'close'.tr,
+            style: HyhTextStyle.body16BoldYellow,
+          ),
         ),
       ],
     );
