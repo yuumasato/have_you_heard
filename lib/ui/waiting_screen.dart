afccc47d8938f3a01d8303f8d8a24dc31d41c88f
Watson Sato <yuuma.sato@gmail.com>
2022-02-25 23:13:07 +0100

    Make side view of the car dynamic
    The car in the waiting screen will show the side view car with the elected persona.
    Or a neutral character if one was not elected yet.

--- lib/ui/waiting_screen.dart
+++ lib/ui/waiting_screen.dart
@@ -103,16 +103,27 @@ class _WaitingScreenState extends State<WaitingScreen>
                         bottom: 0,
                         child: SlideTransition(
                           position: _carEntrance,
-                          child: Container(
-                              height: 82,
-                              width: 120,
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                    image: svg_provider.Svg("assets/images/car_sideview.svg",
-                                        size: Size(628, 417)),
-                                    fit: BoxFit.fitWidth),
-                              )
-                          ),
+                          child: Stack(
+                              children: [
+                                Positioned(
+                                    left: 13,
+                                    top: -20,
+                                    child: Image(
+                                      height: 288/3,
+                                      width: 128/3,
+                                      image: svg_provider.Svg(
+                                        'assets/images/body' +  gc.game.persona.replaceAll(' ', '') + '.svg',
+                                      ),
+                                    )),
+                                Image(
+                                  height: 82,
+                                  width: 120,
+                                  fit: BoxFit.fitWidth,
+                                  image: svg_provider.Svg(
+                                    'assets/images/car_sideview_empty.svg',
+                                  ),
+                                ),
+                              ]),
                         ),
                       ),
                     ]),
